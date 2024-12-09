804d4cdc75e46ab0eb47bc5af27bdb4cac4cc934
uichanOvO <dmlcks74100@gmail.com>
2024-12-09 16:19:47 +0900

    Update 함사울래.js

--- 함사울래.js
+++ 함사울래.js
@@ -1,10 +1,10 @@
-const canvas = document.getElementById("myCanvas1");
-const ctx = canvas.getContext("2d");
-let gradient = ctx.createRadialGradient(350, 200, 150, 300, 120, 10);
-gradient.addColorStop(0, "purple");
-gradient.addColorStop(1, "pink");
+const canvas1 = document.getElementById("myCanvas1");
+const ctx1 = canvas1.getContext("2d");
+let gradient1 = ctx1.createRadialGradient(350, 200, 150, 300, 120, 10);
+gradient1.addColorStop(0, "purple");
+gradient1.addColorStop(1, "pink");
   
-ctx.beginPath();
-ctx.arc(350, 200, 150, 0, Math.PI * 2);
-ctx.fillStyle = gradient;
-ctx.fill();
+ctx1.beginPath();
+ctx1.arc(350, 200, 150, 0, Math.PI * 2);
+ctx1.fillStyle = gradient;
+ctx1.fill();
