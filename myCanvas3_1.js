259dfba109a513f7aa640c8181b5724008964560
uichanOvO <dmlcks74100@gmail.com>
2024-12-09 16:25:34 +0900

    Update myCanvas3_1.js

--- myCanvas3_1.js
+++ myCanvas3_1.js
@@ -1,11 +1,11 @@
-const canvas = document.getElementById("myCanvas3");
-const ctx = canvas.getContext("2d");
+const canvas3 = document.getElementById("myCanvas3");
+const ctx3 = canvas3.getContext("2d");
 
-let gradient = ctx.createRadialGradient(350, 200, 150, 300, 120, 10);
-gradient.addColorStop(0, "pink");
-gradient.addColorStop(1, "white");
+let gradient3 = ctx3.createRadialGradient(350, 200, 150, 300, 120, 10);
+gradient3.addColorStop(0, "pink");
+gradient3.addColorStop(1, "white");
 
-ctx.beginPath()
-ctx.arc(350, 200, 150, 0, Math.PI * 2);
-ctx.fillStyle = gradient;
-ctx.fill();
+ctx3.beginPath()
+ctx3.arc(350, 200, 150, 0, Math.PI * 2);
+ctx3.fillStyle = gradient3;
+ctx3.fill();
