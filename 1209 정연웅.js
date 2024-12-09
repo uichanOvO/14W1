05af0d177e4ba38dbb9c4044b0fe4c88428030c8
jellygas <92729328+jellygas@users.noreply.github.com>
2024-12-09 16:21:20 +0900

    Update 1209 정연웅.js

--- 1209 정연웅.js
+++ 1209 정연웅.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById("myCanvas2");
 const ctx = canvas.getContext("2d");
-
 let gradient = ctx.createRadialGradient(300, 150, 100, 250, 70, 10);
 gradient.addColorStop(0, "black");
 gradient.addColorStop(1, "white");
