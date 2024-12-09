c4e54546cede2f21bee60e7655d6e87292425159
uichanOvO <dmlcks74100@gmail.com>
2024-12-09 16:25:37 +0900

    Update myCanvas4(1).js

--- myCanvas4(1).js
+++ myCanvas4(1).js
@@ -1,11 +1,11 @@
-const canvas = document.getElementById("myCanvas4");
-const ctx = canvas.getContext("2d");
+const canvas4 = document4.getElementById("myCanvas4");
+const ctx4 = canvas4.getContext("2d");
 
-let gradient = ctx.createRadialGradient(350, 200, 150, 300, 120, 10);
-gradient.addColorStop(0, "Orange");
-gradient.addColorStop(1, "Yellow");
+let gradient4 = ctx4.createRadialGradient(350, 200, 150, 300, 120, 10);
+gradient4.addColorStop(0, "Orange");
+gradient4.addColorStop(1, "Yellow");
 
-ctx.beginPath();
-ctx.arc(350, 200, 150, 0, Math.PI * 2);
-ctx.fillStyle = gradient;
-ctx.fill();
+ctx4.beginPath();
+ctx4.arc(350, 200, 150, 0, Math.PI * 2);
+ctx4.fillStyle = gradient4;
+ctx4.fill();
