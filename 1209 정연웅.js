ee43ad46c0f84f1b1094d64071363a7dbc632a54
uichanOvO <dmlcks74100@gmail.com>
2024-12-09 16:25:30 +0900

    Update 1209 정연웅.js

--- 1209 정연웅.js
+++ 1209 정연웅.js
@@ -1,10 +1,10 @@
-const canvas = document.getElementById("myCanvas2");
-const ctx = canvas.getContext("2d");
-let gradient = ctx.createRadialGradient(300, 150, 100, 250, 70, 10);
-gradient.addColorStop(0, "black");
-gradient.addColorStop(1, "white");
+const canvas2 = document.getElementById("myCanvas2");
+const ctx2 = canvas2.getContext("2d");
+let gradient2 = ctx2.createRadialGradient(300, 150, 100, 250, 70, 10);
+gradient2.addColorStop(0, "black");
+gradient2.addColorStop(1, "white");
 
-ctx.beginPath();
-ctx.arc(300, 150, 100, 0, Math.PI * 2);
-ctx.fillStyle = gradient;
-ctx.fill();
+ctx2.beginPath();
+ctx2.arc(300, 150, 100, 0, Math.PI * 2);
+ctx2.fillStyle = gradient2;
+ctx2.fill();
